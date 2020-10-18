c935b51302a1a0828fd10b99ab137503f0fd659d
MeisiLi <1137112460@qq.com>
2020-10-19 10:43:26 +1100

    connect to the backend

--- src/pages/Budget/Budget.js
+++ src/pages/Budget/Budget.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
 
 export default class Budget extends Component {
+    state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+
+  componentDidMount() {
+    this.getItems()
+  }
+
+  getItems = _ => {
+    fetch(`https://ballistic-circular-parent.glitch.me/getall?fbclid=IwAR3I5jHaJwcConYQarRy4lngs-q0ozRyINwpTgWZZRKL-_T5rWeZnHwbtCY`)
+      .then(response => response.json())
+      .then(response => 
+        this.setState({ 
+          isLoaded: true,
+          items: response 
+        }))
+      .catch(error => 
+        this.setState({
+          isLoaded: true,
+          error: error
+        })
+      )
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Budget</h1>
-      </div>
-    )
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          {items.map(item => (
+            <li key={item._id}>
+              ID: {item._id} {item.category} {item.amount} {item.currency} {item.Time}
+            </li>
+          ))}
+        </div>
+      );
+    }
   }
 }
