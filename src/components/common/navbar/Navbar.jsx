06098504269a202deb39f6f03ec3d8d52728b902
Suphachahar <z5176892@ad.unsw.edu.au>
2020-10-19 13:07:28 +1100

    del home nav

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar () {
 
   return (
     <section className="navbar">
-      <a href="/" className="navbar-item">Home</a>
+      
       <a href="/tracker" className="navbar-item">Tracker</a>
       <a href="/budget" className="navbar-item">Budget</a>
       <a href="/export" className="navbar-item">Export</a>
