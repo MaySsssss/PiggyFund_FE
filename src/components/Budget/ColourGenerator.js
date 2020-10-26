d890fab1ae8e2a53aa2d407f13e716d1506df73a
kerubintin <tkifung@yahoo.com.hk>
2020-10-26 16:37:02 +1100

    chart finished

--- src/components/Budget/ColourGenerator.js
+++ src/components/Budget/ColourGenerator.js
@@ -7,24 +7,30 @@ export var ColourGenerator = (function () {
 
     function createInstance(amount) {
         var letters = '89ABCDEF';
-        var result = [];
+        var hoverLetters = '01234567';
+        var colours = [];
+        var hoverColours = [];
         for (var i = 0; i < amount; i++) {
-            var colour = '#';
+            var c = '#';
+            var hc = '#';
             for (var j = 0; j < 6; j++) {
-                colour += letters[Math.floor(Math.random() * 8)];
+                var pos = Math.floor(Math.random() * 8);
+                c += letters[pos];
+                hc += hoverLetters[pos];
             }
 
             var colourOK = true;
-            for (var k = 0; k < result.length; k++) {
-                var existed = result[k];
-                if ((Math.abs(parseInt(existed.slice(1, 3) - colour.slice(1, 3))) + Math.abs(parseInt(existed.slice(3, 5) - colour.slice(3, 5))) + Math.abs(parseInt(existed.slice(5, 7) - colour.slice(5, 7)))) < 20) {
+            for (var k = 0; k < colours.length; k++) {
+                var existed = colours[k];
+                if ((Math.abs(parseInt(existed.slice(1, 3) - c.slice(1, 3))) + Math.abs(parseInt(existed.slice(3, 5) - c.slice(3, 5))) + Math.abs(parseInt(existed.slice(5, 7) - c.slice(5, 7)))) < 20) {
                     colourOK = false;
                     break;
                 }
             }
 
             if (colourOK) {
-                result.push(colour)
+                colours.push(c)
+                hoverColours.push(hc)
             } else {
                 i--;
             }
@@ -32,7 +38,7 @@ export var ColourGenerator = (function () {
         timer = setInterval(function () {
             time--;
         }, 1000);
-        return result;
+        return [colours, hoverColours];
     }
 
     return {
