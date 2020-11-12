3d2b82b4853e925774cc258fa09414000b517fa8
Suphachahar <z5176892@ad.unsw.edu.au>
2020-11-12 17:22:48 +1100

    hover export button

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -49,9 +49,10 @@ function Navbar () {
       <a href="/tracker" className="navbar-item">Tracker</a>
       <a href="/budget" className="navbar-item">Budget</a>
       
-      <div className="navbar-item" onClick={handleClickOpen}>
+      <a className="navbar-item" onClick={handleClickOpen}>
         <GetAppIcon style={{ color: '#404040', fontSize: 24 }}/>
-      </div>
+      </a>
+      
       <Dialog
         fullScreen={fullScreen}
         open={open}
